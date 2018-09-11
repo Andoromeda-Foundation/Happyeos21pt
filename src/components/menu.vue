fa4306a94644a5f4ba65c240b8c934a5f3615bb7
Frank Wei <frank@frankwei.xyz>
2018-09-11 20:53:55 +0800

    feat: dropdown menu

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -7,18 +7,41 @@
       <div id="menu-section-container">
         <el-button type="text" class="menu-item" @click="isShowFairDialog = !isShowFairDialog">{{$t('FAIRNESS')}}</el-button>
         <el-button type="text" class="menu-item" @click="isShowCommunityDialog = !isShowCommunityDialog">{{$t('COMMUNITY')}}</el-button>
+        <el-button type="text" class="menu-item" @click="isShowHowToPlayDialog = !isShowHowToPlayDialog">{{$t('HOW TO PLAY')}}</el-button>      
       </div>
       <div id="menu-section-container">
-        <el-button type="text" class="menu-item" @click="store.currentGame = 'dice'">{{$t('Dice')}}</el-button>
-        <el-button type="text" class="menu-item" @click="store.currentGame = 'blackJack'">{{$t('BlackJack')}}</el-button>
-        <el-button type="text" class="menu-item" @click="store.currentGame = 'otc'">{{$t('Otc')}}</el-button>      
+        <el-dropdown trigger="click" @command="handleMenuClick">
+          <span class="el-dropdown-link">
+            {{store.currentGame}}<i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item command="dice">{{$t('Dice')}}</el-dropdown-item>
+            <el-dropdown-item command="blackJack">{{$t('BlackJack')}}</el-dropdown-item>
+            <el-dropdown-item command="otc">{{$t('OTC')}}</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
       </div>
       <div id="menu-section-container">
-        <el-button type="text" class="menu-item" @click="changeNetwork()">{{store.network}}</el-button>
-        <img src="../assets/flag-ch.jpg" class="lang-icon menu-item" @click="changeLang" v-if="store.lang === 'ch'">
-        <img src="../assets/flag-en.jpg" class="lang-icon menu-item" @click="changeLang" v-else>
+        <el-dropdown trigger="click" @command="handleNetworkChange">
+          <span class="el-dropdown-link">
+            当前网络: {{store.network}}<i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item command="mainnet">{{$t('Mainnet')}}</el-dropdown-item>
+            <el-dropdown-item command="kylin">{{$t('Kylin Testnet')}}</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+        <el-dropdown trigger="click" @command="changeLang">
+          <span class="el-dropdown-link">
+            {{langCodeToReadableStr[store.lang]}} <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item command="ch"> 中文 </el-dropdown-item>
+            <el-dropdown-item command="en"> English</el-dropdown-item>
+            <el-dropdown-item command="jp"> 日本語　</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
         <el-button type="text" class="menu-item" @click="isShowReferralsDialog = !isShowReferralsDialog">{{$t('REFERRALS')}}</el-button>
-        <el-button type="text" class="menu-item" @click="isShowHowToPlayDialog = !isShowHowToPlayDialog">{{$t('HOW TO PLAY')}}</el-button>
       </div>
     </div>
       <!-- 弹出 -->
@@ -83,6 +106,11 @@ export default {
         isShowCommunityDialog: false,
         isShowReferralsDialog: false,
         isShowHowToPlayDialog: false,
+        langCodeToReadableStr: {
+          "ch": "中文",
+          "en": "English",
+          "jp": "日本語",
+        }
       };
     },
   computed: {
@@ -91,13 +119,19 @@ export default {
     }
   },
   methods: {
-    changeNetwork() {
-      const newNetwork = this.store.network === 'kylin' ? 'mainnet' : 'kylin';
+    handleMenuClick(val) {
+      this.store.currentGame = val
+      console.info(val)
+    },
+    handleNetworkChange(val) {
+      this.changeNetwork(val)
+    },
+    changeNetwork(newNetwork) {
       localStorage.setItem('network', newNetwork);
       location.reload();
     },
-    changeLang() {
-      this.store.lang = this.store.lang === 'ch' ? 'en' : 'ch';
+    changeLang(lang) {
+      this.store.lang = lang
       localStorage.setItem('lang', this.store.lang);
       this.$i18n.locale = this.store.lang;
     },
@@ -136,6 +170,12 @@ export default {
 }
 #logo {
   height: 50px;
+}
+#menu .el-dropdown {
+    display: inline-block;
+    position: relative;
+    font-size: 14px;
+    color: #EEE;
 }
 .menu-item {
   color: white;
