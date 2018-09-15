5785d065144a2d83ba674b8974c464f30ff0e280
Deasoso <932152571@qq.com>
2018-09-15 19:35:02 +0800

    add referer.

--- Contracts/Nebulas/dice.js
+++ Contracts/Nebulas/dice.js
@@ -38,14 +38,38 @@ class Operator {
 class CounterContract {
     constructor() {       
         LocalContractStorage.defineProperties(this, {
-            counter: null
+            counter: null,
+            referCut: null,
         })
         LocalContractStorage.defineMapProperties(this, {
         })
     }
 
+   // referer by default is empty
+   bet(referer = "", bet_number = 50, is_under = true) {
+        var {
+            from,
+            value
+        } = Blockchain.transaction
+        
+        this._sendCommissionTo(referer, value)
+
+        var roll_number = Math.floor(Math.random() * 100);        
+        if (is_under) {
+            if (bet_number < roll_number) {
+                Blockchain.transfer(from, value * 96 / bet_number)
+            }
+        } else {
+            if(bet_number > roll_number) {
+                Blockchain.transfer(from, value * 96 / (99 - bet_number))
+            }
+        }
+    }
+
+
     init() {
         this.counter = 0
+        this.referCut = new BigNumber(5)
     }
 
     inc() {
@@ -54,6 +78,17 @@ class CounterContract {
 
     get() {
         return this.counter
+    }
+    
+    _sendCommissionTo(referer, actualCost) {
+        const {
+            referCut
+        } = this
+        if (referer !== "") {
+            const withoutCut = new BigNumber(100).dividedToIntegerBy(referCut)
+            const cut = actualCost.dividedToIntegerBy(withoutCut)
+            Blockchain.transfer(referer, cut)
+        }
     }
 }
 
